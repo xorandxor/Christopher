9317842df42edb3d56bc1033611b4d92baacbc50
paulr <paulr@DESKTOP-GK5FR9T>
2022-07-02 22:27:57 -0600

    Add project files.

---
+++ Christopher/kraken.cs
@@ -0,0 +1,369 @@
+﻿
+using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.WebSockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KrakenCSharpExampleConsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //TODO: UPDATE WITH YOUR KEYS :)
+            string apiPublicKey = "YOUR_PUBLIC_KEY";
+            string apiPrivateKey = "YOUR_PRIVATE_KEY";
+
+            try
+            {
+                System.Console.WriteLine("|=========================================|");
+                System.Console.WriteLine("|        KRAKEN.COM C# TEST APP           |");
+                System.Console.WriteLine("|=========================================|");
+                System.Console.WriteLine();
+
+                #region Public REST API Examples
+
+                if (1 == 0)
+                {
+                    string publicResponse = "";
+
+                    string publicEndpoint = "SystemStatus";
+                    string publicInputParameters = "";
+
+                    //MORE PUBLIC REST EXAMPLES
+                    //*
+
+                    //string publicEndpoint = "AssetPairs";
+                    //string publicInputParameters = "pair=ethusd,xbtusd";
+
+                    //string publicEndpoint = "Ticker";
+                    //string publicInputParameters = "pair=ethusd";
+
+                    //string publicEndpoint = "Trades";
+                    //string publicInputParameters = "pair=ethusd&since=0";
+
+                    //*
+
+                    publicResponse = await QueryPublicEndpoint(publicEndpoint, publicInputParameters);
+                    System.Console.WriteLine(publicResponse);
+
+                }
+
+                #endregion
+
+                #region Private REST API Examples
+
+                if (1 == 0)
+                {
+                    string privateResponse = "";
+
+                    string privateEndpoint = "Balance";
+                    string privateInputParameters = "";
+
+                    //MORE PRIVATE REST EXAMPLES
+                    //*
+
+                    //string privateEndpoint = "AddOrder";
+                    //string privateInputParameters = "pair=xbteur&type=buy&ordertype=limit&price=1.00&volume=1";
+
+                    //string privateEndpoint = "AddOrder"
+                    //string privateInputParameters = "pair=xdgeur&type=sell&ordertype=limit&volume=3000&price=%2b10.0%" //Positive Percentage Example (%2 represtes +, which is a reseved character in HTTP)
+
+                    //string privateEndpoint = "AddOrder"
+                    //string privateInputParameters = "pair=xdgeur&type=sell&ordertype=limit&volume=3000&price=-10.0%" //Negative Percentage Example
+
+                    //string privateEndpoint = "AddOrder"
+                    //string privateInputParameters = "pair=xdgeur&type=buy&ordertype=market&volume=3000&userref=789" //Userref Example
+
+                    //string privateEndpoint = "Balance" //{"error":[]} IS SUCCESS, Means EMPTY BALANCE
+                    //string privateInputParameters = "" 
+
+                    //string privateEndpoint = "QueryOrders" 
+                    //string privateInputParameters = "txid=OFUSL6-GXIIT-KZ2JDJ" 
+
+                    //string privateEndpoint = "AddOrder"
+                    //string privateInputParameters = "pair=xdgusd&type=buy&ordertype=market&volume=5000"
+
+                    //string privateEndpoint = "DepositAddresses"
+                    //string privateInputParameters = "asset=xbt&method=Bitcoin" 
+
+                    //string privateEndpoint = "DepositMethods"
+                    //string privateInputParameters = "asset=eth" 
+
+                    //string privateEndpoint = "WalletTransfer" 
+                    //string privateInputParameters = "asset=xbt&to=Futures Wallet&from=Spot Wallet&amount=0.0045" 
+
+                    //string privateEndpoint = "TradesHistory"
+                    //string privateInputParameters = "start=1577836800&end=1609459200" 
+
+                    //string privateEndpoint = "GetWebSocketsToken" 
+                    //string privateInputParameters = "" 
+
+                    //*
+
+
+                    privateResponse = await QueryPrivateEndpoint(privateEndpoint,
+                                                                 privateInputParameters,
+                                                                 apiPublicKey,
+                                                                 apiPrivateKey);
+                    System.Console.WriteLine(privateResponse);
+
+                }
+
+                #endregion
+
+                #region Public WebSocket API Examples
+
+                //if (1 == 0)
+                //{
+                //    string publicWebSocketURL = "wss://ws.kraken.com/";
+                //    string pulbicWebSocketSubscriptionMsg = "{ \"event\": \"subscribe\", \"subscription\": { \"name\": \"ticker\"}, \"pair\": [ \"XBT/EUR\",\"ETH/USD\" ]}";
+
+                //    //MORE PRUBLIC WEBSOCKET EXAMPLES
+                //    //*
+
+                //    //string pulbicWebSocketSubscriptionMsg = "{ \"event\": \"subscribe\", \"subscription\": { \"interval\": 1440, \"name\": \"ohlc\"}, \"pair\": [ \"XBT/EUR\"]}";
+
+                //    //string pulbicWebSocketSubscriptionMsg = "{ \"event\": \"subscribe\", \"subscription\": { \"name\": \"spread\"}, \"pair\": [ \"XBT/EUR\",\"ETH/USD\" ]}";
+
+                //    //*
+
+                //    await OpenAndStreamWebSocketSubscription(publicWebSocketURL, pulbicWebSocketSubscriptionMsg);
+
+                //}
+
+                #endregion
+
+                #region Private WebSocket API Examples
+
+                //if (1 == 0)
+                //{
+                //    string privateWebSocketURL = "wss://ws-auth.kraken.com/";
+
+                //    //GET AND EXTRACT THE WEBSOCKET TOKEN FORM THE JSON RESPONSE               
+                //    string webSocketRestResponseJSON = await QueryPrivateEndpoint("GetWebSocketsToken", "", apiPublicKey, apiPrivateKey);
+
+                //    var splits = webSocketRestResponseJSON.Split(new string[] { "\"token\":\"", "\"}}" }, StringSplitOptions.None);
+                //    string webSocketToken = splits[1];
+
+                //    string privateWebSocketSubscriptionMsg = "{ \"event\": \"subscribe\", \"subscription\": { \"name\": \"ownTrades\", \"token\": \"#TOKEN#\"}}";
+
+                //    //MORE PRIVATE WEBSOCKET EXAMPLES
+                //    //*
+
+                //    //#TOKEN# IS A PLACEHOLDER
+
+                //    //string privateWebSocketSubscriptionMsg = "{ \"event\": \"subscribe\", \"subscription\": { \"name\": \"openOrders\", \"token\": \"#TOKEN#\"}}";
+
+                //    //string privateWebSocketSubscriptionMsg = "{ \"event\": \"subscribe\", \"subscription\": { \"name\": \"balances\", \"token\": \"#TOKEN#\"}}";
+
+                //    //string addOrderExample =  "{\"event\":\"addOrder\",\"reqid\":1234,\"ordertype\":\"limit\",\"pair\":\"XBT/EUR\",\"token\":\"#TOKEN#\",\"type\":\"buy\",\"volume\":\"1\", \"price\":\"1.00\"}";
+
+                //    //*
+
+                //    //REPLACE PLACEHOLDER WITH TOKEN
+                //    privateWebSocketSubscriptionMsg = privateWebSocketSubscriptionMsg.Replace("#TOKEN#", webSocketToken);
+                //    await OpenAndStreamWebSocketSubscription(privateWebSocketURL, privateWebSocketSubscriptionMsg);
+
+                //}
+
+                #endregion
+
+                System.Console.WriteLine();
+                System.Console.WriteLine("|=========================================|");
+                System.Console.WriteLine("|   END OF PROGRAM, HAVE A NICE DAY :)    |");
+                System.Console.WriteLine("|=========================================|");
+                System.Console.WriteLine();
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine();
+                System.Console.WriteLine("AN EXCEPTION OCCURED :(");
+                System.Console.WriteLine(e.ToString());
+            }
+
+        }
+
+        #region Public REST API Endpoints
+
+        private static async Task<string> QueryPublicEndpoint(string endpointName, string inputParameters)
+        {
+            string jsonData;
+            string baseDomain = "https://api.kraken.com";
+            string publicPath = "/0/public/";
+            string apiEndpointFullURL = baseDomain + publicPath + endpointName + "?" + inputParameters;
+            using (HttpClient client = new HttpClient())
+            {
+                jsonData = await client.GetStringAsync(apiEndpointFullURL);
+            }
+            return jsonData;
+        }
+
+        #endregion
+
+        #region Private REST API Endpoints
+
+        private static async Task<string> QueryPrivateEndpoint(string endpointName,
+                                                               string inputParameters,
+                                                               string apiPublicKey,
+                                                               string apiPrivateKey)
+        {
+            string baseDomain = "https://api.kraken.com";
+            string privatePath = "/0/private/";
+
+            string apiEndpointFullURL = baseDomain + privatePath + endpointName;
+            string nonce = DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString();
+            if (string.IsNullOrWhiteSpace(inputParameters) == false)
+            {
+                inputParameters = "&" + inputParameters;
+            }
+            string apiPostBodyData = "nonce=" + nonce + inputParameters;
+            string signature = CreateAuthenticationSignature(apiPrivateKey,
+                                                             privatePath,
+                                                             endpointName,
+                                                             nonce,
+                                                             inputParameters);
+            string jsonData;
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Add("API-Key", apiPublicKey);
+                client.DefaultRequestHeaders.Add("API-Sign", signature);
+                client.DefaultRequestHeaders.Add("User-Agent", "KrakenDotNet Client");
+                StringContent data = new StringContent(apiPostBodyData, Encoding.UTF8, "application/x-www-form-urlencoded");
+                HttpResponseMessage response = await client.PostAsync(apiEndpointFullURL, data);
+                jsonData = response.Content.ReadAsStringAsync().Result;
+            }
+
+            return jsonData;
+        }
+
+        #endregion
+
+        #region Authentication Algorithm
+
+        public static string CreateAuthenticationSignature(string apiPrivateKey,
+                                                           string apiPath,
+                                                           string endpointName,
+                                                           string nonce,
+                                                           string inputParams)
+        {
+            byte[] sha256Hash = ComputeSha256Hash(nonce, inputParams);
+            byte[] sha512Hash = ComputeSha512Hash(apiPrivateKey, sha256Hash, apiPath, endpointName, nonce, inputParams);
+            string signatureString = Convert.ToBase64String(sha512Hash);
+            return signatureString;
+        }
+
+        private static byte[] ComputeSha256Hash(string nonce, string inputParams)
+        {
+            byte[] sha256Hash;
+            string sha256HashData = nonce.ToString() + "nonce=" + nonce.ToString() + inputParams;
+            using (var sha = SHA256.Create())
+            {
+                sha256Hash = sha.ComputeHash(Encoding.UTF8.GetBytes(sha256HashData));
+            }
+            return sha256Hash;
+        }
+
+        private static byte[] ComputeSha512Hash(string apiPrivateKey,
+                                                byte[] sha256Hash,
+                                                string apiPath,
+                                                string endpointName,
+                                                string nonce,
+                                                string inputParams)
+        {
+
+            string apiEndpointPath = apiPath + endpointName;
+            byte[] apiEndpointPathBytes = Encoding.UTF8.GetBytes(apiEndpointPath);
+            byte[] sha512HashData = apiEndpointPathBytes.Concat(sha256Hash).ToArray();
+            HMACSHA512 encryptor = new HMACSHA512(Convert.FromBase64String(apiPrivateKey));
+            byte[] sha512Hash = encryptor.ComputeHash(sha512HashData);
+            return sha512Hash;
+        }
+
+        #endregion
+
+        #region WebSocket API
+
+        //private static async Task OpenAndStreamWebSocketSubscription(string connectionURL, string webSocketSubscription)
+        //{
+        //    try
+        //    {
+        //        ClientWebSocket webSocketClient = new ClientWebSocket();
+        //        CancellationTokenSource stoppingToken = new CancellationTokenSource();
+
+        //        //OPEN CONNECTION
+        //        await webSocketClient.ConnectAsync(new Uri(connectionURL), CancellationToken.None);
+        //        System.Console.WriteLine("WEBSOCKET CONNECTION OPEN!");
+
+        //        //SEND SUBSCRIPTION MESSAGE
+        //        System.Console.WriteLine("WEBSOCKET SUBSCRIPTION MESSAGE:");
+        //        System.Console.WriteLine(webSocketSubscription);
+        //        if (webSocketClient.State == WebSocketState.Open)
+        //        {
+        //            await webSocketClient.SendAsync(Encoding.UTF8.GetBytes(webSocketSubscription),
+        //                                            WebSocketMessageType.Text,
+        //                                            true,
+        //                                            stoppingToken.Token);
+        //        }
+
+        //        //RECEIVE AND PRINT MESSAGE
+        //        do
+        //        {
+        //            await ReceiveMessage(webSocketClient, stoppingToken.Token);
+        //        } while (webSocketClient.State == WebSocketState.Open);
+
+        //        //CLOSE CONNECTION 
+        //        await webSocketClient.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, "", stoppingToken.Token);
+        //    }
+        //    catch (OperationCanceledException)
+        //    {
+        //        //DO NOTHING - ASSUME WAS REQUESTED
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw ex;
+        //    }
+        //}
+
+        //private static async Task ReceiveMessage(ClientWebSocket socket, CancellationToken stoppingToken)
+        //{
+        //    var buffer = new ArraySegment<byte>(new byte[2048]);
+        //    while (!stoppingToken.IsCancellationRequested)
+        //    {
+
+        //        WebSocketReceiveResult result;
+        //        using (var ms = new MemoryStream())
+        //        {
+        //            do
+        //            {
+        //                result = await socket.ReceiveAsync(buffer, stoppingToken);
+        //                ms.Write(buffer.Array, buffer.Offset, result.Count);
+        //            } while (!result.EndOfMessage);
+
+        //            if (result.MessageType == WebSocketMessageType.Close)
+        //            {
+        //                break;
+        //            }
+
+        //            string wsMsg = "";
+        //            ms.Seek(0, SeekOrigin.Begin);
+        //            using (var reader = new StreamReader(ms, Encoding.UTF8))
+        //            {
+        //                wsMsg = await reader.ReadToEndAsync();
+        //            }
+        //            string msgTime = DateTime.Now.ToString("dd-MMM-yyyy HH:mm:ss") + ": ";
+        //            System.Console.WriteLine(msgTime + wsMsg);
+        //        }
+        //    };
+        //}
+
+        #endregion
+
+    }
+}
---
+++ Monitor/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Monitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.splitter2 = new System.Windows.Forms.Splitter();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(10, 10);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(329, 583);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(134, 100);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(288, 30);
+            this.textBox1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(786, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(10);
+            this.panel1.Size = new System.Drawing.Size(349, 603);
+            this.panel1.TabIndex = 2;
+            // 
+            // splitter1
+            // 
+            this.splitter1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.splitter1.Location = new System.Drawing.Point(783, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 603);
+            this.splitter1.TabIndex = 3;
+            this.splitter1.TabStop = false;
+            // 
+            // splitter2
+            // 
+            this.splitter2.Location = new System.Drawing.Point(0, 0);
+            this.splitter2.Name = "splitter2";
+            this.splitter2.Size = new System.Drawing.Size(3, 603);
+            this.splitter2.TabIndex = 4;
+            this.splitter2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(134, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 23);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "API Server URL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(142, 202);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 23);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "API Server URL";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(142, 228);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(288, 30);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(138, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 23);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "API Server URL";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(138, 165);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(288, 30);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(142, 266);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 23);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Scan Interval";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(142, 292);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(288, 30);
+            this.textBox4.TabIndex = 10;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10000;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(71, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 40);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1135, 603);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.splitter2);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.Splitter splitter2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
