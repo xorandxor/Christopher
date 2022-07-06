4e287881a96e99ff5722f6009b6250fc448b21d2
xorandxor <paulreade738@gmail.com>
2022-07-05 20:38:43 -0600

    convert fields to properties

--- KrakenObjects/KrakenObjects.cs
+++ KrakenObjects/KrakenObjects.cs
@@ -3,46 +3,47 @@
     public class OHLC
     {
     }
+
     /// <summary>
     /// class to hold ticker data from the kraken api
     /// </summary>
     public class Ticker
     {
         //Ask [<price>, <whole lot volume>, <lot volume>]
-        public string[] Ask = new string[3];
+        private string[] ask = new string[3];
 
         //XBTUSD, ETHUSD, etc
-        public string AssetPair = "";
+        private string assetPair = "";
 
         //Bid [<price>, <whole lot volume>, <lot volume>]
-        public string[] Bid = new string[3];
+        private string[] bid = new string[3];
 
         //if errors occurred store them here
-        public string[] Errors = new string[10];
+        private string[] errors = new string[10];
 
         //set to true if error <> null
-        public bool ErrorState = false;
+        private bool errorState = false;
 
         //High [<today>, <last 24 hours>]
-        public string[] High = new string[2];
+        private string[] high = new string[2];
 
         //Last trade closed [<price>, <lot volume>]
-        public string[] LastTradeClosed = new string[2];
+        private string[] lastTradeClosed = new string[2];
 
         //Low [<today>, <last 24 hours>]
-        public string[] Low = new string[2];
+        private string[] low = new string[2];
 
         //Number of trades [<today>, <last 24 hours>]
-        public int[] NumberOfTrades = new int[2];
+        private int[] numberOfTrades = new int[2];
 
         //Today open price
-        public string Open = "";
+        private string open = "";
 
         //Volume [<today>, <last 24 hours>]
-        public string[] Volume = new string[2];
+        private string[] volume = new string[2];
 
         //Volume weighted average price [<today>, <last 24 hours>]
-        public string[] VolumeWeightedAveragePrice = new string[2];
+        private string[] volumeWeightedAveragePrice = new string[2];
 
         public Ticker()
         { }
@@ -69,5 +70,18 @@
             this.High = pHigh;
             this.Open = pOpen;
         }
+
+        public string[] Ask { get => ask; set => ask = value; }
+        public string AssetPair { get => assetPair; set => assetPair = value; }
+        public string[] Bid { get => bid; set => bid = value; }
+        public string[] Errors { get => errors; set => errors = value; }
+        public bool ErrorState { get => errorState; set => errorState = value; }
+        public string[] High { get => high; set => high = value; }
+        public string[] LastTradeClosed { get => lastTradeClosed; set => lastTradeClosed = value; }
+        public string[] Low { get => low; set => low = value; }
+        public int[] NumberOfTrades { get => numberOfTrades; set => numberOfTrades = value; }
+        public string Open { get => open; set => open = value; }
+        public string[] Volume { get => volume; set => volume = value; }
+        public string[] VolumeWeightedAveragePrice { get => volumeWeightedAveragePrice; set => volumeWeightedAveragePrice = value; }
     }
 }
