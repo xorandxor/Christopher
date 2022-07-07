d7f0e6b1581dcc6c12141f8d6806267fc5c5768b
xorandxor <paulreade738@gmail.com>
2022-07-07 17:17:55 -0600

    various fixes

--- KrakenObjects/Order.cs
+++ KrakenObjects/Order.cs
@@ -1,33 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KrakenObjects
 {
-    public class Order
+    public enum BuyOrSellType
     {
-        public Order() { }
+        Buy = 0,
+        Sell = 1
+    }
 
-        public int UserRef = 0;
-        KrakenOrderType OrderType = KrakenOrderType.Market;
-        BuyOrSellType Type = BuyOrSellType.Buy;
-        string Volume = "";
-        string Pair = "";
-        string Price = "";
-        string Price2 = "";
-        LeverageLevel Leverage = LeverageLevel.None;
-        String StpType = "";
-        string OfFlags = "";
-        string TimeInforce = "";
-        string StartTime = "";
-        string ExpireTime = "";
-        KrakenCloseOrderType CloseOrderType = KrakenCloseOrderType.StopLoss;
-        string ClosePrice = "";
-        String ClosePrice2 = "";
-        string Deadline = "";
-        bool Validate = false;
+    public enum KrakenCloseOrderType
+    {
+        None = 0,
+        Limit = 1,
+        StopLoss = 2,
+        TakeProfit = 3,
+        StopLossLimit = 4,
+        TakeProfitLimit = 5
     }
 
     public enum KrakenOrderType
@@ -40,11 +28,6 @@ namespace KrakenObjects
         TakeProfitLimit = 5,
         SettlePosition = 6
     }
-    public enum BuyOrSellType
-    {
-        Buy = 0,
-        Sell = 1
-    }
 
     public enum LeverageLevel
     {
@@ -55,12 +38,48 @@ namespace KrakenObjects
         FiveToOne = 4
     }
 
-    public enum KrakenCloseOrderType
+    public class Order
     {
-        Limit = 0,
-        StopLoss = 1,
-        TakeProfit = 2,
-        StopLossLimit = 3,
-        TakeProfitLimit = 4,
+        private KrakenCloseOrderType closeOrderType = KrakenCloseOrderType.StopLoss;
+        private string closePrice = "";
+        private String closePrice2 = "";
+        private string deadline = "";
+        private string expireTime = "";
+        private LeverageLevel leverage = LeverageLevel.None;
+        private string ofFlags = "";
+        private KrakenOrderType orderType = KrakenOrderType.Market;
+        private string pair = "";
+        private string price = "";
+        private string price2 = "";
+        private string startTime = "";
+        private String stpType = "";
+        private string timeInforce = "";
+        private BuyOrSellType type = BuyOrSellType.Buy;
+        private int userRef = 0;
+        private bool validate = false;
+
+        private string volume = "";
+
+        public Order()
+        { }
+
+        public KrakenCloseOrderType CloseOrderType { get => closeOrderType; set => closeOrderType = value; }
+        public string ClosePrice { get => closePrice; set => closePrice = value; }
+        public string ClosePrice2 { get => closePrice2; set => closePrice2 = value; }
+        public string Deadline { get => deadline; set => deadline = value; }
+        public string ExpireTime { get => expireTime; set => expireTime = value; }
+        public LeverageLevel Leverage { get => leverage; set => leverage = value; }
+        public string OfFlags { get => ofFlags; set => ofFlags = value; }
+        public KrakenOrderType OrderType { get => orderType; set => orderType = value; }
+        public string Pair { get => pair; set => pair = value; }
+        public string Price { get => price; set => price = value; }
+        public string Price2 { get => price2; set => price2 = value; }
+        public string StartTime { get => startTime; set => startTime = value; }
+        public string StpType { get => stpType; set => stpType = value; }
+        public string TimeInforce { get => timeInforce; set => timeInforce = value; }
+        public BuyOrSellType Type { get => type; set => type = value; }
+        public int UserRef { get => userRef; set => userRef = value; }
+        public bool Validate { get => validate; set => validate = value; }
+        public string Volume { get => volume; set => volume = value; }
     }
 }
