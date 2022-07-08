163728697c44d2673d240cacab91bc3ac0344876
xorandxor <paulreade738@gmail.com>
2022-07-08 01:30:58 -0600

    logging

---
+++ Logging/Logging.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+namespace Logging
+{
+    public class Log
+    {
+        public void WriteToEventLog(string sLog, string sSource, string message, EventLogEntryType level)
+        {
+            if (!EventLog.SourceExists(sSource)) EventLog.CreateEventSource(sSource, sLog);
+
+            EventLog.WriteEntry(sSource, message, level);
+        }
+    }
+}
