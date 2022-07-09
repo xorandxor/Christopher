181b031f53b27f7f916b1fe386f74c4dd10fbda5
xorandxor <paulreade738@gmail.com>
2022-07-08 20:20:41 -0600

    Add technical Analysis Class, lots of todo here

---
+++ TechnicalAnalysis/TA.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kraken
+{
+
+    /// <summary>
+    /// Perform Technical Analysis On Kraken Crypto Exchange
+    /// MACD https://api.coin-ta.com/macd?key=lpeiMu4nO3xvSk1&backtracks=5&exchange=kraken&symbol=xbtusdt&interval=15m&fastPeriod=12&slowPeriod=26&signalPeriod=9
+    /// RSI https://api.coin-ta.com/rsi?key=lpeiMu4nO3xvSk1&backtracks=15&exchange=kraken&symbol=xbtusd&interval=15m
+    /// OHLC Data https://api.kraken.com/0/public/OHLC?pair=XBTUSD (gives volume +- as well as last three candle sticks)
+    /// </summary>
+    public static class TechnicalAnalysis
+    {
+        public static string GetRSI()
+        {
+            
+        }
+        public static string GetPriceAccelDecel()
+        {
+
+        }
+
+        public static string GetVolumeChange()
+        {
+
+        }
+
+        public static string GetHttp()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                //client.DefaultRequestHeaders.Clear();
+                //client.DefaultRequestHeaders.Add("API-Key", apiPublicKey);
+                //client.DefaultRequestHeaders.Add("API-Sign", signature);
+                //client.DefaultRequestHeaders.Add("User-Agent", "KrakenDotNet Client");
+                StringContent data = new StringContent(apiPostBodyData, Encoding.UTF8, "application/x-www-form-urlencoded");
+                HttpResponseMessage response = await client.PostAsync(apiEndpointFullURL, data);
+                jsonData = response.Content.ReadAsStringAsync().Result;
+            }
+
+            return jsonData;
+        }
+
+    }
+
+
+
+}
